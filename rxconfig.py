695626ebdbdf7a1d55297fc55308fbae543a488e
Angela Crabtree <angela.crabtree.88@gmail.com>
2025-11-04 09:12:51 -0800

    changed frontend_path in rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,8 @@
 import reflex as rx
+from pathlib import Path 
 
 config = rx.Config(
     app_name="my_portfolio_site",
     cors_allowed_origins=["https://my-portfolio-site-69zu.onrender.com", "http://localhost:3000"],
+    frontend_path=Path(__file__).parent / "frontend"
 )
