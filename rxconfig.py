e784336bff6c988aaf6c9448a79ff33895cf0bc3
Angela Crabtree <angela.crabtree.88@gmail.com>
2025-11-04 09:15:12 -0800

    changed frontend_path to string

--- rxconfig.py
+++ rxconfig.py
@@ -4,5 +4,5 @@ from pathlib import Path
 config = rx.Config(
     app_name="my_portfolio_site",
     cors_allowed_origins=["https://my-portfolio-site-69zu.onrender.com", "http://localhost:3000"],
-    frontend_path=Path(__file__).parent / "frontend"
+    frontend_path=str(Path(__file__).parent / "frontend")
 )
