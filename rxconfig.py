690b6330bfaa569b1d7396f1bcad0b68010c4300
Angela Crabtree <angela.crabtree.88@gmail.com>
2025-11-04 07:45:29 -0800

    updated rxconfig with local host

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="my_portfolio_site",
-    cors_allowed_origins=["https://my-portfolio-site-69zu.onrender.com"],
+    cors_allowed_origins=["https://my-portfolio-site-69zu.onrender.com", "http://localhost:3000"],
 )
