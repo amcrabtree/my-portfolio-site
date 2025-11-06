a0b378cb071fbc261e67e25b57b135793d9981cf
Angela Crabtree <angela.crabtree.88@gmail.com>
2025-11-06 07:50:41 -0800

    mainly updated rxconfig

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,15 @@ from pathlib import Path
 
 config = rx.Config(
     app_name="my_portfolio_site",
-    cors_allowed_origins=["https://my-portfolio-site-69zu.onrender.com", "http://localhost:3000"],
+    cors_allowed_origins=[
+        "https://my-portfolio-site-69zu.onrender.com",
+        "https://my-portfolio-site-1.onrender.com",
+        "http://localhost:3000",
+    ],
+    allowed_hosts=[
+        "https://my-portfolio-site-69zu.onrender.com",
+        "https://my-portfolio-site-1.onrender.com",
+        "http://localhost:3000",
+    ],
     disable_plugins=['reflex.plugins.sitemap.SitemapPlugin'],
 )
