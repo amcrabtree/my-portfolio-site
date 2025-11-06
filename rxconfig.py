85cfbfaf183ea37ad665500d3b131cdfcd7a7d02
Angela Crabtree <angela.crabtree.88@gmail.com>
2025-11-06 10:38:22 -0800

    updated rxconfig

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,6 @@
 import reflex as rx
 from pathlib import Path 
+import os
 
 config = rx.Config(
     app_name="my_portfolio_site",
@@ -15,3 +16,7 @@ config = rx.Config(
     ],
     disable_plugins=['reflex.plugins.sitemap.SitemapPlugin'],
 )
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    rx.run(config, host="0.0.0.0", port=port)
