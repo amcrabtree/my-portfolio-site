9def0a026fa14167dcb00d0ef42208a4f9dca617
Angela Crabtree <angela.crabtree.88@gmail.com>
2025-11-06 10:40:03 -0800

    updated allowed hosts

--- rxconfig.py
+++ rxconfig.py
@@ -5,13 +5,11 @@ import os
 config = rx.Config(
     app_name="my_portfolio_site",
     cors_allowed_origins=[
-        "https://my-portfolio-site-69zu.onrender.com",
-        "https://my-portfolio-site-1.onrender.com",
+        "https://my-portfolio-site-s173.onrender.com",
         "http://localhost:3000",
     ],
     allowed_hosts=[
-        "https://my-portfolio-site-69zu.onrender.com",
-        "https://my-portfolio-site-1.onrender.com",
+        "https://my-portfolio-site-s173.onrender.com",
         "http://localhost:3000",
     ],
     disable_plugins=['reflex.plugins.sitemap.SitemapPlugin'],
