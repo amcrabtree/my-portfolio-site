73e7b4159ea003a65e77ce74624aa04223c05e99
Angela Crabtree <angela.crabtree.88@gmail.com>
2025-11-06 17:47:12 -0800

    updated rxconfig and requirements

--- rxconfig.py
+++ rxconfig.py
@@ -5,6 +5,6 @@ import os
 config = rx.Config(
     app_name="my_portfolio_site",
     allowed_hosts=["my-portfolio-site-s173.onrender.com", "localhost",],
-    cors_allowed_origins=["https://my-portfolio-site-s173.onrender.com", "http://localhost:3000",],
+    cors_allowed_origins=["https://my-portfolio-site-s173.onrender.com", "http://localhost:3000", "http://localhost:10000"],
     disable_plugins=['reflex.plugins.sitemap.SitemapPlugin'],
 )
