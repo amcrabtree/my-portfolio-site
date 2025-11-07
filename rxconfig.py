4a88f0a0f5d0ddf2ca2ae17a1d5640c433dc489d
Angela Crabtree <angela.crabtree.88@gmail.com>
2025-11-06 21:49:57 -0800

    added domain site address to rxconfig

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,16 @@ import reflex as rx
 
 config = rx.Config(
     app_name="my_portfolio_site",
-    allowed_hosts=["my-portfolio-site-s173.onrender.com", "localhost",],
-    cors_allowed_origins=["https://my-portfolio-site-s173.onrender.com", "http://localhost:3000", "http://localhost:10000"],
+    allowed_hosts=[
+        "my-portfolio-site-s173.onrender.com", 
+        "localhost",
+        "www.biodataworks.com",
+        ],
+    cors_allowed_origins=[
+        "https://my-portfolio-site-s173.onrender.com", 
+        "http://localhost:3000", 
+        "http://localhost:10000",
+        "https://www.biodataworks.com",
+        ],
     disable_plugins=['reflex.plugins.sitemap.SitemapPlugin'],
 )
